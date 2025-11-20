fb289712cb0955c6e53f0500d49eed7fc56c9c14
mark <frizenmarkv@gmail.com>
2025-11-20 21:25:57 +0300

    Задание 4 (N2_6)

--- N2_6.py
+++ N2_6.py
@@ -10,21 +10,21 @@ def convert_currency():
     except ValueError:
         result_label.config(text="Ошибка: введите число")
 # Создание основного окна
-root = tk.Tk()
-root.title("Конвертер валют")
-root.geometry("300x200")
+window = tk.Tk()
+window.title("Конвертер валют")
+window.geometry("500x300")
 # Поля ввода
-ttk.Label(root, text="Сумма:").pack(pady=5)
-amount_entry = ttk.Entry(root)
+ttk.Label(window, text="Сумма:").pack(pady=5)
+amount_entry = ttk.Entry(window)
 amount_entry.pack(pady=5)
-ttk.Label(root, text="Курс:").pack(pady=5)
-rate_entry = ttk.Entry(root)
+ttk.Label(window, text="Курс:").pack(pady=5)
+rate_entry = ttk.Entry(window)
 rate_entry.pack(pady=5)
 # Кнопка конвертации
-convert_button = ttk.Button(root, text="Конвертировать", command=convert_currency)
+convert_button = ttk.Button(window, text="Конвертировать", command=convert_currency)
 convert_button.pack(pady=10)
 # Метка для результата
-result_label = ttk.Label(root, text="Результат: ")
+result_label = ttk.Label(window, text="Результат: ")
 result_label.pack(pady=10)
 # Запуск главного цикла
-root.mainloop()
+window.mainloop()
